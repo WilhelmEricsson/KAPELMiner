37aec2227acbb7edcba9566ef60a0f5459bfc78c
Wilhelm Ericsson <wilhelm__ericsson@hotmail.com>
2019-04-27 13:04:18 +0200

    Inner class Position extracted from KAPMiner

--- src/KAPMiner.java
+++ src/KAPMiner.java
@@ -128,52 +128,6 @@ public class KAPMiner {
         }
     }
 
-    private static class Position {
-        private int first, last;
-
-        public Position(int first, int last) {
-            this.first = first;
-            this.last = last;
-        }
-
-        public int getFirst() {
-            return first;
-        }
-
-        public int getLast() {
-            return last;
-        }
-
-        public void expand(Position other) {
-            this.first = Math.min(this.first, other.first);
-            this.last = Math.max(this.last, other.last);
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o)
-                return true;
-            if (o == null || getClass() != o.getClass())
-                return false;
-
-            Position that = (Position) o;
-            if (first != that.first)
-                return false;
-            return last == that.last;
-        }
-
-        @Override
-        public int hashCode() {
-            int result = first;
-            result = 31 * result + last;
-            return result;
-        }
-
-        @Override
-        public String toString() {
-            return "{" + "first=" + first + ", last=" + last + '}';
-        }
-    }
 
     private static class ItemSet {
 
